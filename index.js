86b060709ce114d57318aecc17a1fc0ce7c526d6
lnnd <lnnduy68@gmail.com>
2020-12-11 22:22:28 +0700

    refactor: hi

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ app.post("/uploads", upload.single("image"), (req, res) => {
 });
 
 if (process.env.NODE_ENV !== "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("/client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
   });
 }
 
